d35656ed749bccf9d71f88526ade9bb723dfd542
Klear <kingklear@gmail.com>
2019-06-07 10:55:43 +0200

    Starting with missions.

--- main/planet.lua
+++ main/planet.lua
@@ -2,6 +2,24 @@ local FCrew = require("main.functionsCrew")
 
 local F = {}
 
+local regionStats = {
+	Core = {
+		government = {"Federation"},
+		recruitMax = 5,
+		settlement = {"Megalopolis"}
+	},
+	Frontier = {
+		government = {"Federation", "Rebels", "Independent", "Independent", "Independent"},
+		recruitMax = 3,
+		settlement = {"Outpost"}
+	},
+	Rim = {
+		government = {"Rebels", "Rebels", "Independent", "Independent"},
+		recruitMax = 1,
+		settlement = {"Outpost"}
+	}
+}
+
 function F.generatePlanet(region, start)
 	local planet
 	if start then
@@ -19,9 +37,27 @@ function F.generatePlanet(region, start)
 			table.insert(planet.recruits, FCrew.generateCrew())
 		end
 	else
-
+		planet = {
+			price = {
+				fuel = 50,
+				rations = 20
+			},
+			region = region,
+			recruits = {}
+		}
+		planet.government = regionStats[region].government[math.random(1, #regionStats[region].government)]
+		planet.settlement = regionStats[region].settlement[math.random(1, #regionStats[region].settlement)]
+		local recruitsCount = math.random(0, regionStats[region].recruitMax)
+		for x = 1, recruitsCount do
+			table.insert(planet.recruits, FCrew.generateCrew())
+		end
 	end
 	return planet
+end
+
+local function generateMission()
+
+	return mission
 end
 
 return F
