f9a0ae34a33cbbad039e393b73214380a4d06bb9
Kklear <kingklear@gmail.com>
2019-11-20 14:24:41 +0100

    Button template working, dialog template starting to look good.

---
+++ main/GUI/dialog.go
@@ -0,0 +1,51 @@
+components {
+  id: "dialog"
+  component: "/main/GUI/dialog.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "button"
+  type: "factory"
+  data: "prototype: \"/main/GUI/button_main.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "frame"
+  type: "factory"
+  data: "prototype: \"/main/GUI/dialog_frame.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
